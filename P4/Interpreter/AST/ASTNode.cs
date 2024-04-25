c3cec5edb2dc0afaa476e8936534d39002040844
Rune <rforsb22@student.aau.dk>
2024-04-25 13:29:27 +0200

    Interface added for type

--- P4/Interpreter/AST/ASTNode.cs
+++ P4/Interpreter/AST/ASTNode.cs
@@ -7,6 +7,7 @@ namespace P4.Interpreter.AST;
 // Base class for all AST nodes
 public abstract class ASTNode {
     protected List<ASTNode> Children { get; set; } = new();
+    public abstract T Accept<T>(IASTVisitor<T> visitor);
 }
 
 public class BlockNode : ASTNode {
---
+++ P4/Interpreter/AST/IASTVisitor.cs
@@ -0,0 +1,24 @@
+﻿namespace P4.Interpreter.AST;
+
+public interface IASTVisitor<T>
+{
+    T Visit(ASTNode node);
+    T Visit(BlockNode node);
+    T Visit(FunctionDeclarationNode node);
+    T Visit(VariableDeclarationNode node);
+    T Visit(TypeNode node);
+    T Visit(ExpressionNode node); // binop og unop
+    T Visit(ConstantNode node);
+    T Visit(IdentifierNode node);
+    //T Visit(BinOpNode node);
+    //T Visit(UnaryOpNode node);
+    T Visit(FunctionCallNode node);
+    T Visit(ParameterNode node);
+    T Visit(IfNode node);
+    T Visit(WhileNode node);
+    T Visit(ReturnNode node);
+    T Visit(ForNode node);
+    
+
+    
+}
