55789eb289bb5613d50b6fdc7dda9f4f593dc5ea
JacobStokholms <Stokholm.jacob@gmail.com>
2024-05-06 10:56:14 +0200

    Beginning of ScopeTypeChecker

---
+++ P4/Interpreter/ScopeTypeChecker.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using P4.Interpreter.AST;
+
+namespace P4.Interpreter;
+
+public class ScopeTypeChecker : IASTVisitor<Type>
+{
+    private SymbolTable _symbolTableType = new();
+
+    public Type Visit(ASTNode node)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Type Visit(BlockStatement node)
+    {
+        foreach (var statement in node.Statements)
+        {
+            Visit(statement);
+        }
+        return null;
+    }
+
+    public Type Visit(BinaryExpression node)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Type Visit(UnaryExpression node)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Type Visit(TernaryExpression node)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    
+    public Type Visit(AssignmentStatement node)
+    {
+        // Scope rule
+        if (!_symbolTableType.IsVariableDeclared(node.VariableName))
+        {
+            throw new Exception("Variable not declared.");
+        }
+        //Type checking
+        Type expression = Visit(node.Expression);
+        
+        if (!_symbolTableType.IsTypeCorrect(node.VariableName, expression))
+        {
+            throw new Exception("Type mismatch.");
+        }
+
+        return null;
+    }
+
+    public Type Visit(FunctionCallStatement node)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Scope rule
+    public Type Visit(VariableDeclaration node)
+    {
+        var varName = node.VariableName;
+        if (_symbolTableType.IsVariableDeclaredInScope(varName))
+        {
+            throw new Exception($"Variable '{varName}' already declared.");
+        }
+
+        var varType = node.Type;
+        _symbolTableType.Add(varName, varType);
+        return null;
+    }
+   
+
+    public Type Visit(Expression node)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Type Visit(Statement node)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Type Visit(ConstantExpression node)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Type Visit(IdentifierExpression node)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Type Visit(ParameterNode node)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Type Visit(FunctionDeclaration node)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Type Visit(PrintStatement node)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Type Visit(IfBlock node)
+    {
+        _symbolTableType.PushScope(); // Enter new scope for the if block
+        Visit(node.Block); // Visit children
+        _symbolTableType.PopScope(); // Exit scope
+        return null;
+    }
+
+    public Type Visit(WhileBlock node)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Type Visit(ReturnStatement node)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Type Visit(ForLoopStatement node)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ P4/Interpreter/SymbolTable.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P4.Interpreter;
+
+public class SymbolTable
+{
+    private readonly Stack<Dictionary<string, object>> _symbolTable = new();
+
+    public SymbolTable()
+    {
+        // Initialize with a global scope
+        _symbolTable.Push(new Dictionary<string, object>());
+    }
+
+    public void Add(string name, object value)
+    {
+        _symbolTable.Peek().Add(name, value);
+    }
+
+    public object Get(string name)
+    {
+        foreach (var scope in _symbolTable)
+        {
+            if (scope.ContainsKey(name))
+            {
+                return scope[name];
+            }
+        }
+
+        throw new Exception($"Variable {name} not found");
+    }
+    public bool IsVariableDeclared(string varName)
+    {
+        foreach (var scope in _symbolTable)
+        {
+            if (scope.ContainsKey(varName))
+                return true;
+        }
+        return false;
+    }
+    
+    public bool IsVariableDeclaredInScope(string varName)
+    {
+            if (_symbolTable.Peek().ContainsKey(varName))
+                return true;
+            return false;
+    }
+    public bool IsTypeCorrect(string varName, object value)
+    {
+        foreach (var scope in _symbolTable)
+        {
+            if (scope.ContainsKey(varName))
+            {
+                return scope[varName].GetType() == value.GetType();
+            }
+        }
+        return false;
+    }
+
+    public void PushScope()
+    {
+        _symbolTable.Push(new Dictionary<string, object>());
+    }
+
+    public void PopScope()
+    {
+        _symbolTable.Pop();
+    }
+}
