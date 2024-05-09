ce0436143f7180f0d387c97c72a4a3c2a3279450
JacobReeslev <jreesl22@student.aau.dk>
2024-05-09 12:44:50 +0200

    More Unit tests

---
+++ P4/Interpreter/Tests/ScopeTypeCheckerTests/VisitTernaryExpression_Should.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace P4.Interpreter.Tests.ScopeTypeCheckerTests;
+using Xunit;
+using P4.Interpreter.AST;
+
+public class VisitTernaryExpression_Should
+{
+    private readonly ScopeTypeChecker _checker = new();
+    
+    [Fact]
+    public void Visit_TernaryExpression_ReturnsType_Success()
+    {
+        var condition = new ConstantExpression(true);
+        var trueExpression = new ConstantExpression(1);
+        var falseExpression = new ConstantExpression(2);
+        var node = new TernaryExpression(condition, trueExpression, falseExpression);
+        var result = _checker.Visit(node);
+        
+        Assert.Equal("Num", result.TypeName);
+    }
+    
+    [Fact]
+    public void Visit_TernaryExpression_conditionNum_ThrowsException()
+    {
+        var condition = new ConstantExpression(9);
+        var trueExpression = new ConstantExpression(1);
+        var falseExpression = new ConstantExpression(2);
+        var node = new TernaryExpression(condition, trueExpression, falseExpression);
+        
+        Assert.Throws<Exception>(() => _checker.Visit(node));
+    }
+
+    [Fact]
+    public void Visit_TernaryExpression_conditionStringAndNum_ThrowsException()
+    {
+        var condition = new ConstantExpression(true);
+        var trueExpression = new ConstantExpression("test");
+        var falseExpression = new ConstantExpression(2);
+        
+        var node = new TernaryExpression(condition, trueExpression, falseExpression);
+        
+        Assert.Throws<Exception>(() => _checker.Visit(node));
+    }
+}
