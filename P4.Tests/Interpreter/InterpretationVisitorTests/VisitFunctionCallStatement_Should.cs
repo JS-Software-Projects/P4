2edbe789c82faecf2d978c35efe8c56c6e103bb9
Rasmus <112973664+Altazhar@users.noreply.github.com>
2024-05-15 00:01:38 +0200

    Added unit tests and changed switch case

---
+++ P4.Tests/Interpreter/InterpretationVisitorTests/VisitFunctionCallStatement_Should.cs
@@ -0,0 +1,8 @@
+﻿using P4.Interpreter;
+
+namespace P4.Tests.Interpreter.InterpretationVisitorTests;
+
+public class VisitFunctionCallStatement_Should
+{
+    private readonly InterpretationVisitor _visitor = new();
+}
---
+++ P4.Tests/Interpreter/InterpretationVisitorTests/VisitIfBlock_Should.cs
@@ -0,0 +1,8 @@
+﻿using P4.Interpreter;
+
+namespace P4.Tests.Interpreter.InterpretationVisitorTests;
+
+public class VisitIfBlock_Should
+{
+    private readonly InterpretationVisitor _visitor = new();
+}
