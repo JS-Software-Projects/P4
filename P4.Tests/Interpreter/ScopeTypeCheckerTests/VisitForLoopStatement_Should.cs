d51cdd47d4febccc8b47e243039a0553fbd993d6
Rune <rforsb22@student.aau.dk>
2024-05-14 13:13:55 +0200

    Recovered unit tests from type scope checker

---
+++ P4.Tests/Interpreter/ScopeTypeCheckerTests/VisitForLoopStatement_Should.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using P4.Interpreter.AST.Nodes;
+
+namespace P4.Interpreter.Tests.ScopeTypeCheckerTests;
+using Xunit;
+using P4.Interpreter.AST;
+
+public class VisitForLoopStatement_Should
+{
+    private readonly ScopeTypeChecker _checker = new();
+    
+    [Fact]
+    public void Visit_ForLoopStatement_ReturnsType_Success()
+    {
+        var initialization = new Statement();
+        var condition = new ConstantExpression(true);
+        var increment = new Statement();
+        var block = new BlockStatement();
+
+        var node = new ForLoopStatement(initialization, condition, increment, block);
+        var result = _checker.Visit(node);
+        
+        Assert.Equal(null, result);
+    }
+    
+    [Fact]
+    public void Visit_ForLoopStatement_conditionNum_ThrowsException()
+    {
+        var initialization = new Statement();
+        var condition = new ConstantExpression(9);
+        var increment = new Statement();
+        var block = new BlockStatement();
+
+        var node = new ForLoopStatement(initialization, condition, increment, block);
+        
+        Assert.Throws<Exception>(() => _checker.Visit(node));
+    }
+}
---
+++ P4.Tests/Interpreter/ScopeTypeCheckerTests/VisitIfBlock_Should.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using P4.Interpreter.AST;
+using P4.Interpreter.AST.Nodes;
+using Xunit;
+
+
+namespace P4.Interpreter.Tests.ScopeTypeCheckerTests;
+
+public class VisitIfBlock_Should
+{
+    private readonly ScopeTypeChecker _checker = new();
+
+    
+    [Fact]
+    public void Visit_IfBlock_ConditionTypeIsBool_Success()
+    {
+        // Arrange
+        var condition = new ConstantExpression(true);
+        var block = new BlockStatement();  
+        var elseBlock = new BlockStatement();
+        var node = new IfBlock(condition, block, elseBlock);
+
+        // Act
+        var result = _checker.Visit(node);
+
+        // Assert
+        Assert.Equal(null, result);
+    }
+    
+    [Fact]
+    public void Visit_IfBlock_ConditionTypeIsNotBool_ThrowsException()
+    {
+        // Arrange
+        var condition = new ConstantExpression(1);
+        var block = new BlockStatement();  
+        var elseBlock = new BlockStatement();
+        var node = new IfBlock(condition, block, elseBlock);
+
+        // Act+Assert
+        Assert.Throws<Exception>(() => _checker.Visit(node));
+    }
+}
