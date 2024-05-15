c768e4c86a0ff22cd80c5f1808ea9d1a111a20d3
JacobReeslev <jreesl22@student.aau.dk>
2024-05-15 13:07:05 +0200

    UnitTest

---
+++ P4.Tests/Interpreter/EnvironmentTests/EnvironmentAdd_should.cs
@@ -0,0 +1,38 @@
+﻿using P4.Interpreter;
+using Environment = P4.Interpreter.Environment;
+
+namespace P4.Tests.Interpreter.EnvironmentTests;
+using Xunit;
+
+public class EnvironmentAdd_should
+{
+   private readonly Environment _environment = new();
+
+   [Fact]
+    public void Add_AddsValueToEnvironment_Success()
+    {
+        // Arrange
+        var name = "testVar";
+        var value = 1;
+        _environment.DeclareVariable(name);
+        
+        
+        // Act
+        _environment.Add(name, value);
+    
+        // Assert
+        Assert.Equal(value, _environment.Get(name));
+    }
+   
+    [Fact]
+    public void Add_AddsValueToEnvironment_ThrowsException()
+    {
+        // Arrange
+        var name = "testVar";
+        var value = 1;
+        
+        // Act + Assert
+        Assert.Throws<Exception>(() => _environment.Add(name, value));
+    }
+   
+}
